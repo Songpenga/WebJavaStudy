387eb5b756c8698334c0ac75dfea33f7043d45b1
Songpenga <iiuuyy6654@gmail.com>
2022-10-25 21:11:33 +0900

    출력 test

---
+++ src/main/java/j01_출력/Test1.java
@@ -0,0 +1,16 @@
+package j01_출력;
+
+/**
+ * @
+ */
+public class Test1 {
+    public static void main(String[] args){
+        //문자열 출력
+        System.out.print("이름: \n");
+        System.out.println("AS ");
+        System.out.print("주소:"); // 한 라인으로 출력
+        System.out.println("부산 센텀");
+
+        boolean Isusernamechecflage = true; // 카멜 alt shift U -> 헝가리안
+    }
+}
