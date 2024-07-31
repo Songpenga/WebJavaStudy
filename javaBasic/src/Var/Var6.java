99fb0f13644a95aff5387235306723cca3cdea7e
Songpenga <iiuuyy6654@gmail.com>
2024-07-31 12:00:06 +0900

    240731 변수

---
+++ javaBasic/src/Var/Var6.java
@@ -0,0 +1,10 @@
+package Var;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a); // 컴파일에러 발생
+        // --> java: variable a might not have been initialized
+        // 해석 : 변수가 초기화 되지 않음
+    }
+}
---
+++ javaBasic/src/Var/Var7.java
@@ -0,0 +1,11 @@
+package Var;
+
+public class Var7 {
+    public static void main(String[] args) {
+        char a = 'A'; // 문자하나
+        String b = "test"; // 문자열, ''하면 안되고 ""를 써야됨
+/*
+        int z = "TEST"; //java.lang.String cannot be converted to int
+        System.out.println(z);*/
+    }
+}
