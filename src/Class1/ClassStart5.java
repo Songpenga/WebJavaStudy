c13d51a3db62f96014c2a21a222dc87df2ec3ca0
Songpenga <iiuuyy6654@gmail.com>
2025-02-11 17:52:00 +0900

    Update ClassStart5.java

--- src/Class1/ClassStart5.java
+++ src/Class1/ClassStart5.java
@@ -15,15 +15,24 @@ public class ClassStart5 {
         stu2.age = 16;
         stu2.grade = 82;
 
-        Student[] students = new Student[2];
-        students[0] = stu1;
-        students[1] = stu2;
+        Student[] students = new Student[]{stu1, stu2};
 
         /*System.out.println("0 : " + students[0]);
         System.out.println("1 : " + students[1]);*/
 
+/*
         for(int i = 0; i < students.length; i++){
             System.out.println("이름 : " + students[i].name + " 나이 : " + students[i].age + " 성적 : " + students[i].grade);
+        }
+*/
+
+        for(int i = 0; i < students.length; i++){
+            Student s = students[i];
+            System.out.println("이름 : " + s.name + " 나이 : " + s.age + " 성적 : " + s.grade);
+        }
+
+        for (Student s : students){ // iter
+            System.out.println("이름 : " + s.name + " 나이 : " + s.age + " 성적 : " + s.grade+"   A");
         }
 
 /*        System.out.println("이름 : " + students[0].name + " 나이 : " + students[0].age + " 성적 : " + students[0].grade);
