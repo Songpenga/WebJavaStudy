2862820414dd82ad21ab2d2efeae7843f7a9f0f8
Songpenga <iiuuyy6654@gmail.com>
2025-03-10 15:09:41 +0900

    Update DataCountMain3.java

--- src/section8/static1/DataCountMain3.java
+++ src/section8/static1/DataCountMain3.java
@@ -15,11 +15,13 @@ public class DataCountMain3 {
         //추가
         //인스턴스를 통한 접근
         Data3 data4 = new Data3("D");
-        System.out.println(data4.count);
-        // => Static member 'section8.static1.Data3.count' accessed via instance reference
+        System.out.println(data4.count); // 권장하지 않음
+            //정적 변수의 경우 인스턴스를 통한 접근은 추천하지 않는다.
+            //왜냐면 코드를 읽을 떄 마치 인스턴스 변수에 접근하는 것 처럼 오해할 수 있기 때문이다.
+            // => Static member 'section8.static1.Data3.count' accessed via instance reference
 
         //클래스를 통한 접근
-        System.out.println(Data3.count);
+        System.out.println(Data3.count); // 공용변수에 바로 접근
 
 
     }
