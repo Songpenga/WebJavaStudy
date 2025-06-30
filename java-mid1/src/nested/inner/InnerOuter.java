6ea788e6aec3a221e049b22a698d6b01bb42db31
Peng <iiuuyy6654@gmail.com>
2025-06-30 20:03:56 +0900

    250630

--- java-mid1/src/nested/inner/InnerOuter.java
+++ java-mid1/src/nested/inner/InnerOuter.java
@@ -7,4 +7,25 @@ package com.example.nested.inner;
 *  내부클래스
 */
 public class InnerOuter {
+
+    private static int outClassVal = 3;
+    private int outInstanceVal = 3;
+
+    class Inner{
+        private int innerInstanceVal = 7;
+
+        public void print(){
+            // 자기 자신에 접근
+            System.out.println(innerInstanceVal);
+
+            //외부 클래스의 인스턴스 멤버에 접근 가능, private도 접근 가능
+            System.out.println(outInstanceVal);
+
+            //외부 클래스의 클래스 멤버에 접근 가능, private도 접근 가능
+            System.out.println(outClassVal);
+        }
+
+    }
+
+
 }
