0c877bece4c91fff8cf5d6abca8dab438bcc5551
Peng <iiuuyy6654@gmail.com>
2025-07-03 00:13:42 +0900

    250703

---
+++ java-mid1/src/nested/inner/ex1/CarMain.java
@@ -0,0 +1,8 @@
+package com.example.nested.inner.ex1;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car mycar = new Car("Model Peng", 1000);
+        mycar.start();
+    }
+}
