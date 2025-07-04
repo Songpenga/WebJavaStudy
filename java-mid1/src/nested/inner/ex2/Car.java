ab63e6550ce4b024cb13b3eaa59f1dfd5f16ece8
Peng <iiuuyy6654@gmail.com>
2025-07-04 18:10:04 +0900

    내부클래스

---
+++ java-mid1/src/nested/inner/ex2/Car.java
@@ -0,0 +1,37 @@
+package com.example.nested.inner.ex2;
+
+public class Car {
+
+    private String model;
+    private int chargeLevel;
+    private Engine engine;
+
+    public Car(String model, int chargeLevel) {
+        this.model = model;
+        this.chargeLevel = chargeLevel;
+        this.engine = new Engine();
+    }
+
+    public int getChargeLevel() {
+        return chargeLevel;
+    }
+
+    //Engine에서만 사용하는 메서드
+    public String getModel() {
+        return model;
+    }
+    
+    public void start(){
+        engine.start();
+        System.out.println(model + " 시작완료");
+    }
+    public class Engine {
+
+        public void start(){
+            System.out.println("충전 레벨 확인 : " + chargeLevel);
+            System.out.println(model + "의 엔진을 구동합니다.");
+
+        }
+    }
+
+}
---
+++ java-mid1/src/nested/inner/ex2/CarMain.java
@@ -0,0 +1,9 @@
+package com.example.nested.inner.ex2;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        Car car = new Car("Model peng!", 1000);
+        car.start();
+    }
+}
