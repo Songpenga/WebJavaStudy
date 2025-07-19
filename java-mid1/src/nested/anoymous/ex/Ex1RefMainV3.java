350612dbad69d4e77e2d19e180a1634eb9346d44
Peng <iiuuyy6654@gmail.com>
2025-07-19 23:58:56 +0900

    Create Ex1RefMainV3.java

---
+++ java-mid1/src/nested/anoymous/ex/Ex1RefMainV3.java
@@ -0,0 +1,42 @@
+package com.example.nested.anoymous.ex;
+
+import java.util.Random;
+
+//section9.익명클래스 활용3, 지역클래스 사용
+public class Ex1RefMainV3 {
+
+    public static void hello(Process process){
+        System.out.println("프로그램 시작");
+        process.run();
+        System.out.println("프로그램 종료");
+    }
+
+public static void main(String[] args) {
+    Process dice = new Process(){
+
+        @Override
+        public void run() {
+            //코드 조각 시작
+            int randomVal = new Random().nextInt(6)+1;
+            System.out.println("주사위  = " + randomVal);
+            //코드조각 종료
+        }
+    };
+
+    // 제어자 'static'은(는) 허용되지 않습니다
+    Process sum = new Process(){
+
+        @Override
+        public void run() {
+            int randomVal = new Random().nextInt(6)+1;
+            //코드 조각 시작
+            for(int i = 1; i <= 3; i++){
+                System.out.println("i = " + i);
+            }
+            //코드조각 종료
+        }
+    };
+        hello(dice);
+        hello(sum);
+    }
+}
