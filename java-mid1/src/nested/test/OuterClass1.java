4bb7e684c6e277fd56e24d4a08fd9cb3fbd23f7d
Peng <iiuuyy6654@gmail.com>
2025-07-24 23:41:03 +0900

    Create OuterClass1.java

---
+++ java-mid1/src/nested/test/OuterClass1.java
@@ -0,0 +1,12 @@
+package com.example.nested.test;
+
+public class OuterClass1 {
+
+    static class NestedClass{
+        public void hello(){
+            System.out.println("Nested Class hello!");
+        }
+    }
+
+
+}
