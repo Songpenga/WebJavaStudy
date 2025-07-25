51c709a31df71c7dc86e333514adc5a425b13715
Peng <iiuuyy6654@gmail.com>
2025-07-25 18:51:07 +0900

    Create OuterClass1Main.java

---
+++ java-mid1/src/nested/test/OuterClass1Main.java
@@ -0,0 +1,11 @@
+package com.example.nested.test;
+
+// section9.문제와풀이 정적중첩클래스
+public class OuterClass1Main {
+    public static void main(String[] args) {
+        OuterClass1.NestedClass test = new OuterClass1.NestedClass();
+
+        test.hello();
+
+    }
+}
