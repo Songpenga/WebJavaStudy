bdcf2d2526de78477e7b65735008731d9d5e7f85
Peng <iiuuyy6654@gmail.com>
2025-07-27 21:26:55 +0900

    Create OuterClass2.java

---
+++ java-mid1/src/nested/test/OuterClass2.java
@@ -0,0 +1,9 @@
+package com.example.nested.test;
+
+public class OuterClass2 {
+    class InnerClass{
+        public void hello(){
+            System.out.println("Inner Class hello");
+        }
+    }
+}
