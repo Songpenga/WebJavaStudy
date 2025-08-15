58e9c7b8a903edc0a993caef7d61d44a5c9c21c5
Peng <iiuuyy6654@gmail.com>
2025-08-15 23:49:19 +0900

    Create OuterClass3Main.java

---
+++ java-mid1/src/nested/test/OuterClass3Main.java
@@ -0,0 +1,10 @@
+package com.example.nested.test;
+
+// section9.문제와풀이 지역클래스를 완성해라
+public class OuterClass3Main {
+    public static void main(String[] args) {
+
+        OuterClass3 outerClass3 = new OuterClass3();
+        outerClass3.myMethod();
+    }
+}
