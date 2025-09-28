0ea9157ee741b5ccfd2e33403e9bc97b569abbac
Peng <iiuuyy6654@gmail.com>
2025-09-28 23:52:45 +0900

    Create Comment.java

---
+++ test/test/src/main/java/com/example/test/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.example.test.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    
+    @Column(nullable = false, length = 50)
+    private String author;
+    
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
+
