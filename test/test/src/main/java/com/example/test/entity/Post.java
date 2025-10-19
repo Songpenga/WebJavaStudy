a78fbffc45a17bde50479533d7e66e5b76d50961
Peng <iiuuyy6654@gmail.com>
2025-10-19 20:49:14 +0900

    Create Post.java

---
+++ test/test/src/main/java/com/example/test/entity/Post.java
@@ -0,0 +1,52 @@
+package com.example.test.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 200)
+    private String title;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    
+    @Column(nullable = false, length = 50)
+    private String author;
+    
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    
+    @Column(name = "view_count", nullable = false)
+    private Integer viewCount = 0;
+    
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+    
+    public void incrementViewCount() {
+        this.viewCount++;
+    }
+}
+
